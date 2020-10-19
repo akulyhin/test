2978bb66ec78db83e57fcebb9ccdfdd00eeba74d
Artem Kulyhin <akulyhin@gmail.com>
2020-10-19 23:23:19 +0300

    Last updates

--- newsapi/src/index.js
+++ newsapi/src/index.js
@@ -12,7 +12,7 @@ let page = 0;
 refs.btnSearch.addEventListener('click', () => {
     page = 1;
     query = refs.search.value;
-    const url = `https://newsapi.org/v2/everything?q=${query}&language=ru&page=${page}`;
+    const url = `https://newsapi.org/v2/everything?q=${query}&language=en&page=${page}`;
     const options = {
         headers: {
             Authorization: apiKey,
@@ -20,31 +20,52 @@ refs.btnSearch.addEventListener('click', () => {
     }
 
     refs.ulArticles.innerHTML = '';
+    refs.spinner.classList.remove('is-hidden');
 
     fetch(url, options).then(res => res.json())
     .then(data =>  {
         console.log(data),
         refs.ulArticles.insertAdjacentHTML('beforeend', template(data.articles)),
-    page += 1}
+    page += 1,
+    refs.spinner.classList.add('is-hidden')
+}
     );
     refs.btnMore.classList.remove('is-hidden')
 })
 
 
+
 refs.btnMore.addEventListener('click', () => {
-    const url = `https://newsapi.org/v2/everything?q=${query}&language=ru&page=${page}`;
+    const url = `https://newsapi.org/v2/everything?q=${query}&language=en&page=${page}`;
     const options = {
         headers: {
             Authorization: apiKey,
         },
     }
-
+    refs.spinner.classList.remove('is-hidden');
+    refs.btnText.textContent = 'Загрузка...';
+    refs.btnSpinner.classList.remove('is-hidden');
+    refs.btnMore.disabled = true;
     page = 1;
 
     fetch(url, options).then(res => res.json())
     .then(data =>  {
         console.log(data),
         refs.ulArticles.insertAdjacentHTML('beforeend', template(data.articles)),
-    page += 1}
+    page += 1,
+    refs.btnText.textContent = 'Показать еще',
+    refs.btnSpinner.classList.add('is-hidden'),
+    refs.btnMore.disabled = false,
+    refs.spinner.classList.add('is-hidden'),
+    console.log(data)
+}
     );
+})
+
+
+refs.backToTop.addEventListener('click', () => {
+    scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
 })
--- newsapi/src/js/refs.js
+++ newsapi/src/js/refs.js
@@ -2,7 +2,11 @@ const refs = {
     search: document.getElementById('searchArticles'),
     btnSearch: document.querySelector('.search'),
     ulArticles: document.querySelector('.articles'),
-    btnMore: document.querySelector('[data-action="open-more"]')
+    btnMore: document.querySelector('[data-action="open-more"]'),
+    btnText: document.querySelector('.btnText'),
+    btnSpinner: document.querySelector('.btnSpinner'),
+    backToTop: document.querySelector('.back-top'),
+    spinner: document.querySelector('.spinner-grow')
 }
 
 export default refs;
