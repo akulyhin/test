9e19e50e11f8ad122eb386f71830b468e15366dd
Artem Kulyhin <akulyhin@gmail.com>
2020-10-19 17:01:04 +0300

    Add search request

--- newsapi/src/index.js
+++ newsapi/src/index.js
@@ -1,1 +1,21 @@
 import './styles.css';
+import refs from './js/refs';
+import template from './templates/template.hbs';
+
+console.log(refs.search);
+
+let query = '';
+const apiKey = 'aa8103c96a5c481a951da99868cd8cbc';
+ 
+refs.btnSearch.addEventListener('click', () => {
+    console.log(refs.search.value)
+    query = refs.search.value;
+    const url = `https://newsapi.org/v2/everything?q=${query}&from=2020-09-19&sortBy=publishedAt&apiKey=${apiKey}`;
+    fetch(url).then(res => res.json())
+    .then(data => refs.ulArticles.innerHTML = template(data.articles)
+    );
+
+})
+
+
+// http://newsapi.org/v2/top-headlines?country=us&category=business&
---
+++ newsapi/src/js/refs.js
@@ -0,0 +1,7 @@
+const refs = {
+    search: document.getElementById('searchArticles'),
+    btnSearch: document.querySelector('.search'),
+    ulArticles: document.querySelector('.articles')
+}
+
+export default refs;
